2e4a217aae4b3122d90c76bdce52f4907b0a6f09
Matthew Eric Bassett <mebassett@gegn.net>
2024-01-12 01:45:00 +0000

    partial training - at least the 2nd weights.
    the first layer of weights isn't done yet.  I need to write a function
    for that derivative.
    
    it's really slow.
    
    just using the 2nd weights doesn't help the mean error much!

--- train.cpp
+++ train.cpp
@@ -61,6 +61,7 @@ Training_Data load_data_from_file(string filename) {
     
       }
       current_row = {};
+      if(rows.size() > 100) return rows;
   }
   return rows;
 }
@@ -116,19 +117,34 @@ double layer_0_activation_prime(double x) {
   return 1 - pow(layer_0_activation(x), 2);
 }
 
+double layer_1_activation(double x) {
+  return (exp(x)  - exp(-x)) / ( exp(x) + exp(-x) );
+}
+
+double layer_1_activation_prime(double x) {
+  return 1 - pow(layer_1_activation(x), 2);
+}
+
 
 double layer_0_output(Model_Weights& weights, unsigned int j, valarray<double>& input) {
   return layer_0_activation((weights.layer0_weights[j] * input).sum());
 }
 
-double layer_1_output(Model_Weights& weights, unsigned int j, valarray<double>& input) {
+valarray<double> all_layer_0_outputs(Model_Weights& weights, valarray<double>& input) {
     valarray<double> output_0(weights.num_hidden_nodes);
 
     for(int i =0; i< weights.num_hidden_nodes; i++) {
         output_0[i] = layer_0_output(weights, i, input);
     }
+    return output_0;
 
-    return layer_0_activation( (weights.layer1_weights[j] * output_0).sum() );
+}
+
+double layer_1_output(Model_Weights& weights, unsigned int j, valarray<double>& input) {
+    valarray<double> output_0 = all_layer_0_outputs(weights, input);
+
+
+    return layer_1_activation( (weights.layer1_weights[j] * output_0).sum() );
 }
 
 valarray<double> model_output(Model_Weights& weights, valarray<double>& input) {
@@ -141,7 +157,6 @@ valarray<double> model_output(Model_Weights& weights, valarray<double>& input) {
 
 int from_model_output(valarray<double>& out) {
   for(int i=0; i<out.size(); i++) {
-    cout << "data: " << out[i] << " max: " << out.max() << "\n";
     if(out[i] == out.max()) return i;
   }
   return -1;
@@ -160,11 +175,54 @@ double model_error(Training_Data& data, Model_Weights& weights) {
 
 }
 
+double del_err_by_del_weight_1_I_J (Model_Weights& weights, int i, int j, Training_Datum& row, double activation_result) {
+    double sigma_j = layer_1_output(weights, j, row.x);
+    return - ( row.t[j] - sigma_j) 
+           * activation_result // layer_1_activation_prime( ( all_layer_0_outputs(weights, row.x) * weights.layer1_weights[j]  ).sum()  ) 
+           * layer_0_output(weights, i, row.x);
+}
+
+bool train_layer1_weights(Model_Weights& weights, Training_Datum& row, double learning_rate) {
+    for(int j=0;j<weights.layer1_weights.size(); j++) {
+        valarray<double> grad_err_by_layer_1 (weights.num_hidden_nodes);
+        double activation_result = layer_1_activation_prime( ( all_layer_0_outputs(weights, row.x) * weights.layer1_weights[j]  ).sum()  ) ;
+        for(int i =0; i<weights.num_hidden_nodes;i++) {
+            grad_err_by_layer_1 = del_err_by_del_weight_1_I_J(weights, i, j, row, activation_result);
+        }
+
+        weights.layer1_weights[j] += learning_rate * grad_err_by_layer_1;
+
+    }
+
+
+    return true;
+}
+
+
 
 
 int main(void) {
     Training_Data rows = load_data_from_file("mnist_train.txt");
-    Model_Weights weights = initiate_weights(INPUT_SIZE, 10, OUTPUT_SIZE);
+    Model_Weights weights = initiate_weights(INPUT_SIZE, 100, OUTPUT_SIZE);
+
+
+    cout << "model mean squared error on training data: " << model_error(rows, weights) << "\n";
+    for(auto row : rows) { // = rows[0];;) {
+
+        valarray<double> model_out = model_output(weights, row.x);
+
+        cout << from_model_output(model_out) << " : " << row.y << "\n";
+
+    }
+    for(int round=0; round<100; round++) {
+        cout << "training round: " << round << "\n";
+        for(auto row : rows) {
+            train_layer1_weights(weights, row, 0.01);
+        }
+        cout << "round finished.\n";
+    }
+
+    cout << "model mean squared error on training data: " << model_error(rows, weights) << "\n";
 
     for(auto row : rows) { // = rows[0];;) {
 
@@ -174,7 +232,6 @@ int main(void) {
 
     }
 
-    cout << "model mean squared error on training data: " << model_error(rows, weights);
 
     return 0;
 }
