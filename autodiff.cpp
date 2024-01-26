25a92e8e78b8092d4f081ec78c13e538219b5425
Matthew Eric Bassett <me@mebassett.info>
2024-01-26 01:37:31 +0000

    wip wip using vardic args and sets of pointers.
    this is so I can do
    
    AD z = z + y ....;
    
    z(1, 2, 3, 4);
    
    using pointers for the order of the vars with the hopes (!?) that
    that'll match the order I've declared them in.
    
    so far one problem is with vardic args I can't see how many there are,
       so I can't use different behaviors on having args or not for my
       function call operator.

--- autodiff.cpp
+++ autodiff.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 #include<valarray>
+#include<set>
+#include<cstdarg>
 
 using namespace std;
 
 struct AD {
-     virtual double operator()() = 0;
+     virtual double operator()(...) = 0;
+
+     double val = 0;
+     void setValue( double _val) {
+         this->val = _val;
+     }
+     set<AD*> deps;
 };
 
 struct AD_Constant: AD {
   double val;
-  AD_Constant(double val): val(val) {}
+  AD_Constant(double val): val(val) {
+      this->deps = {};
+  }
 
-  double operator()() { return this->val; }
+  double operator()(...) { return this->val; }
 };
 
 struct AD_Plus: AD {
   AD &augend, &addend;
 
-  AD_Plus(AD& _augend, AD& _addend): augend(_augend), addend(_addend) {}
+  AD_Plus(AD& _augend, AD& _addend): augend(_augend), addend(_addend) {
+      this->deps = {};
+      this->deps.insert(_augend.deps.begin(), _augend.deps.end());
+      this->deps.insert(_addend.deps.begin(), _addend.deps.end());
+  }
 
-  double operator()() {
-    return this->augend() + this->addend();
+
+  double operator()(...) {
+      va_list args;
+      va_start(args, 0);
+      cout << "length : " << this->deps.size() << "\n";
+      for(auto var : this->deps){
+        double val = va_arg(args, double);
+        var->setValue(val);
+      };
+      va_end(args);
+      return this->augend() + this->addend();
   }
 
 
@@ -31,23 +54,34 @@ struct AD_Plus: AD {
 struct AD_Mul: AD {
     AD &plier, &plicand;
 
-    AD_Mul(AD& _plier, AD& _plicand): plier(_plier), plicand(_plicand) {}
+    AD_Mul(AD& _plier, AD& _plicand): plier(_plier), plicand(_plicand) {
+      this->deps = {};
+      this->deps.insert(_plicand.deps.begin(), _plicand.deps.end());
+      this->deps.insert(_plier.deps.begin(), _plier.deps.end());
+    }
 
-    double operator()() {
-        return this->plier() * this->plicand();
+    double operator()(...) {
+      va_list args;
+      va_start(args, 0);
+      cout << "length : " << this->deps.size() << "\n";
+      for(auto var : this->deps){
+        double val = va_arg(args, double);
+        var->setValue(val);
+      };
+      va_end(args);
+      return this->plier() * this->plicand();
     }
 };
 
 
 struct AD_Variable : AD {
-  double val = 0;
-  AD_Variable (double _val): val(_val) {}
-
-  void setValue(double _val) {
+  AD_Variable (double _val) {
+      this->deps = {this};
       this->val = _val;
   }
 
-  double operator()() {
+
+  double operator()(...) {
       return this->val;
   }
 
@@ -73,6 +107,6 @@ int main() {
 
     AD_Mul d = d_ * z ;
 
-    cout << "hello, world!\n" << d() << "\n";
+    cout << "hello, world!\n" << d(5.0,9.0,2) << "\n";
     return 0;
 }
